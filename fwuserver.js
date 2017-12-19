f5698f788f095b58001f58f596e5cfa8ff55d8b1
Tao Wang <tao.wangtaoau@gmail.com>
2017-12-19 14:19:06 +1100

    Server framework initialized and SwaggerAPI integrated

---
+++ fwuserver.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var swaggerUi = require('swagger-ui-express');
+var swaggerDocument = require('./swagger.json');
+
+var index = require('./routes/index');
+var subjects = require('./routes/subjects');
+var port = 3000;
+
+var app = express();
+
+//View Engine
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine', 'ejs');
+app.engine('html',require('ejs').renderFile);
+
+//Set Static Folder (React)
+app.use(express.static(path.join(__dirname,'client')));
+
+//Body Parse Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/',index);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api',subjects);
+
+
+app.listen(port,function(){
+	console.log('Server started on port' + port);
+});
